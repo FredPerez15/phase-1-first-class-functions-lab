ac7223d7b7641b96a4818b95c78e11d1c7cc4b1c
Freddy Perez <fred.perez15@gmail.com>
2023-02-23 22:39:10 +0200

    first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+        return multiplier * fare
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+// `selectDifferentDrivers()` — This function takes two arguments, an array of
+//   the names of Scuber's `drivers` and either the `returnFirstTwoDrivers()` or
+//   `returnLastTwoDrivers()` function. Based on these two arguments,
+//   `selectDifferentDrivers()` will return either the first two drivers or the
+//   last two drivers.
+
+function selectDifferentDrivers(drivers, driverGroup) {
+    return driverGroup(drivers)
+}
